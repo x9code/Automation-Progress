2073720c9206fe305e11e92a92e51fc6104ba61b
x9code <kumarranad672@gmail.com>
2025-10-24 09:46:35 +0530

    css selector

---
+++ src/test/java/qsp/CssSelector.java
@@ -0,0 +1,22 @@
+package qsp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CssSelector {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.get("https://www.flipkart.com/");
+		Thread.sleep(2000);
+		driver.findElement(By.name("q")).sendKeys("Iphone 17");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("button[type ='submit']")).click();
+		Thread.sleep(5000);
+		driver.close();
+	}
+
+}
