4262326352a60bf66cbc04033cb4a1207b7a8268
x9code <kumarranad672@gmail.com>
2025-10-25 10:10:29 +0530

    auto

---
+++ src/test/java/qsp/XpathLocator1.java
@@ -0,0 +1,22 @@
+package qsp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathLocator1 {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.get("https://www.youtube.com/");
+		driver.findElement(By.name("search_query")).sendKeys("bollywood songs 90s");
+		driver.findElement(By.xpath("//button[@title='Search']")).click();
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//a[@id='video-title']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@data-tooltip-title='Full screen (f)']")).click();
+		Thread.sleep(5000);
+		driver.close();
+	}
+}
