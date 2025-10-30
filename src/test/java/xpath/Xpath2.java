f35047b0876eed14c2233610b04f71998ada5d4e
x9code <kumarranad672@gmail.com>
2025-10-30 22:28:59 +0530

    d4

---
+++ src/test/java/xpath/Xpath2.java
@@ -0,0 +1,25 @@
+package xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xpath2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.get("https://www.youtube.com/");
+		driver.findElement(By.name("search_query")).sendKeys("stranger things season 5 trailer");
+		driver.findElement(By.xpath("//button[@title='Search']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("video-title")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//button[@data-tooltip-title='Full screen (f)']")).click();
+		Thread.sleep(20000);
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
