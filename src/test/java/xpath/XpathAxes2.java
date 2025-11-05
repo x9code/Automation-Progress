3be4d74d01d99e12d480401701f30f457bf41b9a
x9code <kumarranad672@gmail.com>
2025-11-05 08:46:16 +0530

    d9

---
+++ src/test/java/xpath/XpathAxes2.java
@@ -0,0 +1,12 @@
+package xpath;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathAxes2 {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("");
+	}
+}
