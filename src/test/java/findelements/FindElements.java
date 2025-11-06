58ec835e7ccc3240e904b2584ddcd4eabc7b2031
x9code <kumarranad672@gmail.com>
2025-11-06 09:26:26 +0530

    d10

---
+++ src/test/java/findelements/FindElements.java
@@ -0,0 +1,25 @@
+package findelements;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElements {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		List<WebElement> allLinks = driver.findElements(By.xpath("//a"));
+		int count = allLinks.size();
+		System.out.println(count);
+		for(int i = 0;i<count;i++) {
+			System.out.println(allLinks.get(i).getText());
+			Thread.sleep(50);
+		}
+		driver.quit();
+		
+	}
+}
