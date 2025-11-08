0b5ab3ce4f11e4fb5dcea1bdd490203d3a00f089
x9code <kumarranad672@gmail.com>
2025-11-08 09:37:06 +0530

    dropdown

--- src/test/java/listbox/ListDropCombobox.java
+++ src/test/java/listbox/ListDropCombobox.java
@@ -1,7 +1,33 @@
 package listbox;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class ListDropCombobox {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		Thread.sleep(4000);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(3000);
+		//click on create new account
+		driver.findElement(By.linkText("Create new account")).click();
+		Thread.sleep(3000);
+		WebElement day = driver.findElement(By.id("day"));
+		Select s1 = new Select(day);
+		s1.selectByIndex(12);
+		Thread.sleep(2000);
+		WebElement month = driver.findElement(By.id("month"));
+		Select s2 = new Select(month);
+		s2.selectByIndex(11);
+		Thread.sleep(2000);
+		WebElement year = driver.findElement(By.id("year"));
+		Select s3 = new Select(year);
+		s3.selectByVisibleText("2002");
+		Thread.sleep(5000);
+		driver.quit();
 	}
 }
