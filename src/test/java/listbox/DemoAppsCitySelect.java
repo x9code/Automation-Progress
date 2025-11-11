52ac4251f86ffe1d0720cfb03a44395e59303667
x9code <kumarranad672@gmail.com>
2025-11-11 08:58:02 +0530

    name changed

--- src/test/java/listbox/DemoAppsCitySelect.java
+++ src/test/java/listbox/DemoAppsCitySelect.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class DemoApps {
+public class DemoAppsCitySelect {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
