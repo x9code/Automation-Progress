ad832f34f11bafd57a0dae2dea9c9d3f67a09fad
x9code <kumarranad672@gmail.com>
2025-11-13 11:26:44 +0530

    WebDriverWait

---
+++ src/test/java/synchronization/ExplicitWait.java
@@ -0,0 +1,23 @@
+package synchronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoapps.qspiders.com/ui?scenario=1");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.titleContains("Text"));
+		driver.findElement(By.xpath("//section[text()='Dropdown']")).click();
+		Thread.sleep(3000);
+		driver.quit();
+	
+	}
+}
