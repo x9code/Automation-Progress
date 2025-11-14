0d6d6189c5fae9a3866e7068e834314fa41c5b51
x9code <kumarranad672@gmail.com>
2025-11-14 22:57:30 +0530

    implicit wait practice

---
+++ src/test/java/daily/ImplicitlyWait1.java
@@ -0,0 +1,22 @@
+package daily;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitlyWait1 {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.flipkart.com/");
+		WebElement ele = driver.findElement(By.name("q"));
+		ele.sendKeys("pens");
+		ele.sendKeys(Keys.ENTER);
+		driver.quit();
+	}
+}
