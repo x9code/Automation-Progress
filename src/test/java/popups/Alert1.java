d6950ade8a746eccab6b2f71d11f21d9f86f1bcb
x9code <kumarranad672@gmail.com>
2025-11-15 09:12:42 +0530

    ALERT1

---
+++ src/test/java/popups/Alert1.java
@@ -0,0 +1,24 @@
+package popups;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alert1 {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://demoapps.qspiders.com/ui/alert?sublist=0");
+		driver.findElement(By.xpath("(//input[@type='checkbox'])[3]")).click();
+		driver.findElement(By.id("deleteButton")).click();
+		Alert a = driver.switchTo().alert();
+		System.out.println(a.getText());
+		a.accept();
+		System.out.println("Deleted.....");
+		driver.quit();
+	}
+}
