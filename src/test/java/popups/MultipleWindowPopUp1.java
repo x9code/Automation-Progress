cc0d72f2982e4f912f78e2e7a1c7ea63dd4f3fc0
x9code <kumarranad672@gmail.com>
2025-11-19 09:16:22 +0530

    multiWindow Popup

--- src/test/java/popups/MultipleWindowPopUp1.java
+++ src/test/java/popups/MultipleWindowPopUp1.java
@@ -7,24 +7,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BrowserWindowPopUp1 {
+public class MultipleWindowPopUp1 {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://demoapps.qspiders.com/ui/browser?sublist=0");
 		String winid = driver.getWindowHandle();
-		System.out.println(winid);
-		driver.findElement(By.xpath("(//p[@class='text-gray-500 mt-2']/following-sibling::button)[1]")).click();
+		System.out.println("parent window id : "+winid);
+		driver.findElement(By.linkText("Multiple Windows")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[text()='Shop Now']")).click();
+		Thread.sleep(1000);
 		Set<String> allWindows = driver.getWindowHandles();
-		for (String win : allWindows) {
-			if(!win.equals(winid)) {
-				driver.switchTo().window(win);
-				break;
-			}
+		int a =1;
+		for (String string : allWindows) {
+			System.out.println(a +" - "+string);
+			a++;
 		}
-		driver.findElement(By.xpath("//button[text()='Add to Cart']")).click();
-		System.out.println("item addded to cart....");
 		Thread.sleep(4000);
 		driver.quit();
 	}
