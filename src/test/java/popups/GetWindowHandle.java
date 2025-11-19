3223593aff0e129970d7a89cf2e9472ed2fd293e
x9code <kumarranad672@gmail.com>
2025-11-19 09:22:12 +0530

    getWindowHandle

--- src/test/java/popups/GetWindowHandle.java
+++ src/test/java/popups/GetWindowHandle.java
@@ -3,12 +3,12 @@ package popups;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class WindowPOPUP {
+public class GetWindowHandle {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
 		System.out.println("driver.getWindowHandle(): "+driver.getWindowHandle());
-		driver.quit();
+		driver.quit(); 
 	}
 }
