d2cbaf82db62ccf448633194ace2a53c02415f11
x9code <kumarranad672@gmail.com>
2025-11-22 10:34:11 +0530

    right-click

---
+++ src/test/java/mouse_actions/Right_click.java
@@ -0,0 +1,31 @@
+package mouse_actions;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Right_click {
+	public static void main(String[] args) throws InterruptedException, AWTException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(2000);
+		WebElement pass = driver.findElement(By.linkText("Forgot your password?"));
+		Actions a = new Actions(driver);
+		Thread.sleep(2000);
+		a.contextClick(pass).perform();
+		Thread.sleep(2000);
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_T);
+		Thread.sleep(4000);
+		driver.quit();
+	}
+}
