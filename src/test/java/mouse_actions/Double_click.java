e06abc58a5cb6cd9d6c56d48de620c57b5a85f2f
x9code <kumarranad672@gmail.com>
2025-11-22 10:10:24 +0530

    double click

--- src/test/java/mouse_actions/Double_click.java
+++ src/test/java/mouse_actions/Double_click.java
@@ -1,5 +1,25 @@
 package mouse_actions;
 
-public class Double_click {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Double_click {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://demoapps.qspiders.com/ui/button/buttonDouble?sublist=2");
+		WebElement but = driver.findElement(By.xpath("//button[text()='No']"));
+		Actions q = new Actions(driver);
+		q.doubleClick(but).perform();
+		Thread.sleep(2000);
+		String txt = driver.findElement(By.xpath("//span[@class='text-green-600 p-1 px-2 ms-2 rounded-md']")).getText();
+		System.out.println(txt);
+		driver.quit();
+	}
 }
