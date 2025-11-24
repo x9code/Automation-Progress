deac6fd9c1a8ef771dcd5230a64dc036e742ba53
x9code <kumarranad672@gmail.com>
2025-11-24 10:06:35 +0530

    scroll to bottom

---
+++ src/test/java/mouse_actions/ScrollAction3.java
@@ -0,0 +1,21 @@
+package mouse_actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScrollAction3 {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.bbc.com/");
+		Thread.sleep(2000);
+		int y = driver.findElement(By.linkText("Content Index")).getLocation().getY();
+		Thread.sleep(2000);
+		JavascriptExecutor j = (JavascriptExecutor) driver;
+		j.executeScript("window.scrollBy(0,"+y+")");
+		Thread.sleep(4000);
+		driver.quit();
+	}
+}
