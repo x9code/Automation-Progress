7eec60048678915ab3a129504df374afbdda25d1
x9code <kumarranad672@gmail.com>
2025-11-26 23:42:37 +0530

    ddt workflow

---
+++ src/test/java/DataDrivenTesting/DataDrivenFlow.java
@@ -0,0 +1,54 @@
+package DataDrivenTesting;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import all_methods.Methods1;
+import all_methods.ReadPropertyFiles;
+
+public class DataDrivenFlow {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		ReadPropertyFiles m = new ReadPropertyFiles();
+		String url = m.readDataFromProperty("url");
+		String un = m.readDataFromProperty("username");
+		String pwd = m.readDataFromProperty("password");
+		driver.get(url);
+		driver.findElement(By.id("username")).sendKeys(un);
+		driver.findElement(By.name("pwd")).sendKeys(pwd);
+		driver.findElement(By.id("loginButton")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[.='Tasks']")).click();
+		driver.findElement(By.xpath("//div[@class='addNewButton']")).click();
+		driver.findElement(By.xpath("//div[.='New Customer']")).click();
+		
+		FileInputStream file = new FileInputStream("D:\\eclipse\\Automation\\testdata\\customer_data.xlsx");
+		Workbook wb = WorkbookFactory.create(file);
+		Sheet sh = wb.getSheet("Customer Data");
+		Row row = sh.getRow(1);
+		Cell cell = row.getCell(1);
+		String username = cell.getStringCellValue();
+		driver.findElement(By.xpath("//div[@class='customerNameDiv']/child::input[@placeholder='Enter Customer Name']")).sendKeys(username);
+		Thread.sleep(2000);
+		Row row1 = sh.getRow(1);
+		Cell cell1 = row1.getCell(2);
+		String des = cell1.toString();
+		driver.findElement(By.xpath("//div[@class='inputContainer']/child::textarea[@placeholder='Enter Customer Description']")).sendKeys(des);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='greyButton cancelBtn']")).click();
+		Thread.sleep(5000);
+		driver.quit();
+	}
+}
