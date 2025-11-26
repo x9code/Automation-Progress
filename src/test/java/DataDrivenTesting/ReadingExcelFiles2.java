267440476425753747aab066a694bf8ada0e1ce6
x9code <kumarranad672@gmail.com>
2025-11-26 10:44:40 +0530

    ddt2

---
+++ src/test/java/DataDrivenTesting/ReadingExcelFiles2.java
@@ -0,0 +1,25 @@
+package DataDrivenTesting;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadingExcelFiles2 {
+	public static void main(String[] args) throws IOException {
+		FileInputStream file = new FileInputStream("D:\\eclipse\\Automation\\testdata\\customer_data.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(file);
+		XSSFSheet sheet = wb.getSheet("Customer Data");
+		int rows = sheet.getLastRowNum();
+		short cells = sheet.getRow(1).getLastCellNum();
+		//System.out.println(rows +" "+cells);
+		for(int r = 0;r<=rows;r++) {
+			XSSFRow currentRow = sheet.getRow(r);
+			for(int c = 0 ; c < cells ; c++) {
+				System.out.printf("%-40s",currentRow.getCell(c).toString());
+			}
+			System.out.println();
+		}
+	}
+}
