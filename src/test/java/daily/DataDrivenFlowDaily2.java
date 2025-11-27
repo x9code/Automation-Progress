b11c69acbfe275e7bd9defc9c2be5e913d697f3f
x9code <kumarranad672@gmail.com>
2025-11-27 09:51:05 +0530

    ddt workbook flow

--- src/test/java/daily/DataDrivenFlowDaily2.java
+++ src/test/java/daily/DataDrivenFlowDaily2.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import all_methods.Methods1;
 import all_methods.ReadPropertyFiles;
 
-public class DataDrivenFlowDaily {
+public class DataDrivenFlowDaily2 {
 	public static void main(String[] args) throws IOException, InterruptedException, AWTException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -36,8 +36,8 @@ public class DataDrivenFlowDaily {
 		driver.findElement(By.id("loginButton")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//span[.='Tasks']")).click();
-		driver.findElement(By.xpath("//div[@class='addNewButton']")).click();
-		driver.findElement(By.xpath("//div[.='New Customer']")).click();
+//		driver.findElement(By.xpath("//div[@class='addNewButton']")).click();
+//		driver.findElement(By.xpath("//div[.='New Customer']")).click();
 		
 		FileInputStream file = new FileInputStream("D:\\eclipse\\Automation\\testdata\\customer_data.xlsx");
 		Workbook wb = WorkbookFactory.create(file);
@@ -47,26 +47,31 @@ public class DataDrivenFlowDaily {
 		Robot r = new Robot();
 		for(int r1 = 1;r1 <= allrows;r1++) {
 			Row cr = sh.getRow(r1);
+			driver.findElement(By.xpath("//div[@class='addNewButton']")).click();
+			driver.findElement(By.xpath("//div[.='New Customer']")).click();
 			for(int c = 1 ; c <= 1 ; c++) {
+				
 				String username = cr.getCell(c).toString();
+				Thread.sleep(2000);
 				driver.findElement(By.xpath("//div[@class='customerNameDiv']/child::input[@placeholder='Enter Customer Name']")).sendKeys(username);
 				String desc = cr.getCell(c+1).toString();
-				System.out.println(username+"            "+desc);
+				//System.out.println(username+"            "+desc);
+				Thread.sleep(2000);
 				driver.findElement(By.xpath("//div[@class='inputContainer']/child::textarea[@placeholder='Enter Customer Description']")).sendKeys(desc);
 				Thread.sleep(1000);
-				r.keyPress(KeyEvent.VK_CONTROL);
-				r.keyPress(KeyEvent.VK_A);
-				r.keyRelease(KeyEvent.VK_CONTROL);
-				Thread.sleep(1000);
-				driver.findElement(By.xpath("//div[@class='customerNameDiv']/child::input[@placeholder='Enter Customer Name']")).clear();
-				r.keyPress(KeyEvent.VK_BACK_SPACE);
-				Thread.sleep(1000);
-				//WebElement descrip = driver.findElement(By.xpath("//div[@class='inputContainer']/child::textarea[@placeholder='Enter Customer Description']"));
-				
-				
+				driver.findElement(By.xpath("(//div[@class='components_button_label'])[9]")).click();
+				Thread.sleep(2000);
+//				r.keyPress(KeyEvent.VK_CONTROL);
+//				r.keyPress(KeyEvent.VK_A);
+//				r.keyRelease(KeyEvent.VK_CONTROL);
+//				Thread.sleep(1000);
+//				driver.findElement(By.xpath("//div[@class='customerNameDiv']/child::input[@placeholder='Enter Customer Name']")).clear();
+//				r.keyPress(KeyEvent.VK_BACK_SPACE);
+//				Thread.sleep(1000);
+//				//driver.findElement(By.xpath("//div[@class='inputContainer']/child::textarea[@placeholder='Enter Customer Description']")).clear();
 			}
 		}
-		driver.findElement(By.xpath("//div[@class='greyButton cancelBtn']")).click();
+		driver.findElement(By.xpath("//div[@class='components_button_label']")).click();
 		Thread.sleep(2000);
 		driver.quit();
 	}
