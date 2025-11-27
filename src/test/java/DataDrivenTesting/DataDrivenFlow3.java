01851ba6c951b1ac8bb4c03c39963a4375d3c66c
x9code <kumarranad672@gmail.com>
2025-11-27 10:23:30 +0530

    read the serial no from excel

---
+++ src/test/java/DataDrivenTesting/DataDrivenFlow3.java
@@ -0,0 +1,27 @@
+package DataDrivenTesting;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import all_methods.Methods1;
+import all_methods.ReadExcelFiles;
+import all_methods.ReadPropertyFiles;
+
+public class DataDrivenFlow3 {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ReadExcelFiles ex = new ReadExcelFiles();
+		String serialNo = ex.readExcelfile("Customer Data", 2, 0);
+		String name = ex.readExcelfile("Customer Data", 2, 1);
+		System.out.println(serialNo +"  "+name);
+	}
+}
