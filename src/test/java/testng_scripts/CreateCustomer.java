b618ecd87ccc7ab2883844fda15ca01326661f34
x9code <kumarranad672@gmail.com>
2025-11-29 10:23:35 +0530

    TestNg - giving priority to test scripts

--- src/test/java/testng_scripts/CreateCustomer.java
+++ src/test/java/testng_scripts/CreateCustomer.java
@@ -3,14 +3,24 @@ package testng_scripts;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+
+
 public class CreateCustomer {
-	@Test
+	@Test (priority = 2)
 	public void createCustomer() {
-		System.out.println("sop..");
-		Reporter.log("reporter...",true);
-		Reporter.log("reporter...",false);
-		Reporter.log("reporter...");
+		Reporter.log("created Customer",true);
 	}
-	
+	@Test(priority = 4)
+	public void deleteCustomer() {
+		Reporter.log("deleted Customer",true);
+	}
+	@Test (priority = 3)
+	public void updateCustomer() {
+		Reporter.log("updated Customer",true);
+	}
+	@Test(priority = 1)
+	public void addCustomer() {
+		Reporter.log("added Customer",true);
+	}
 	
 }
