b4e8891dc484d44d14391757be9689f81a87a3dd
Deepak <contact.deepakrana.work@gmail.com>
2025-12-01 11:16:21 +0530

    groupd

--- src/test/java/testng_scripts/UpdateCustomer.java
+++ src/test/java/testng_scripts/UpdateCustomer.java
@@ -7,20 +7,20 @@ import org.testng.annotations.Test;
 
 
 public class UpdateCustomer {
-	@Test (priority = 2, dependsOnMethods = "deleteCustomer" )
+	@Test (priority = 2 , groups = "smoke")
 	public void createCustomer() {
 		Reporter.log("created Customer",true);
 	}
-	@Test(priority = 4)
+	@Test(priority = 4 , groups = "regression")
 	public void deleteCustomer() {
 		//Assert.fail();
 		Reporter.log("deleted Customer",true);
 	}
-	@Test (priority = 3, invocationCount = 3)
+	@Test (priority = 3, invocationCount = 3, groups = "functionality")
 	public void updateCustomer() {
 		Reporter.log("updated Customer",true);
 	}
-	@Test(priority = 1)
+	@Test(priority = 1, groups = {"regression","smoke"})
 	public void addCustomer() {
 		Reporter.log("added Customer",true);
 	}
