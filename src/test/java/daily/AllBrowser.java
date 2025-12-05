6db3bab9362fe9363bbab21825194c36c301c12f
Deepak <contact.deepakrana.work@gmail.com>
2025-12-05 11:40:33 +0530

    different browsers

---
+++ src/test/java/daily/AllBrowser.java
@@ -0,0 +1,25 @@
+package daily;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AllBrowser {
+    public static void main(String[] args) {
+
+        WebDriver driver;
+
+        System.out.println("Launching Chrome...");
+        driver = new ChromeDriver();
+        driver.quit();
+
+        System.out.println("Launching Firefox...");
+        driver = new FirefoxDriver();
+        driver.quit();
+
+        System.out.println("Launching Edge...");
+        driver = new EdgeDriver();   // This should launch Edge
+        driver.quit();
+    }
+}
